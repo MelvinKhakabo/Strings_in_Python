36b8552487cdf8c6f92093592884fbd67e1db9ed
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-21 14:14:05 +0300

    This file contains practice on python strings

---
+++ practice.py
@@ -0,0 +1,40 @@
+#adding string variables
+name = "Melvin"
+print(name)
+
+#using triple comments for longer messages
+message = """
+we clawed
+we chained
+our hearts in vain
+"""
+print(message)
+
+#compare
+vitamins = ('C', 'B', 'D', 'K')
+grades = ('A', 'B', 'C', 'D',)
+print(vitamins == grades)
+
+#adding strings
+greet = "Hello,"
+name = "Melvin"
+result = greet + name
+print(result)
+
+#looping
+greet = "Hello"
+for letter in greet:
+    print(letter)
+
+#length of a string
+greet = "Hello"
+print(len(greet))
+
+#membership
+greet = "Hello"
+print('ell' in greet)
+
+#getting indexes
+greet = "Hello"
+print(greet.index('o'))
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+This repo contains practice on python strings.
+
+The content includes, creating string variables, using triple quotes for commenting longer messages and mutiline comments in python, comparing strings using comparison operators, adding different strings, looping strings, finding the length of a string and different indexes on string and membership test for strings.
+
+Feel free to clone the repo and practice on your end.
